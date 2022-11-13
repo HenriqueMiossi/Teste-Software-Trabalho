c8531c165294a396303e19d0d79f89e619660b9f
HenriqueMiossi <henriquemiossi@gmail.com>
2022-11-13 20:18:12 -0300

    Finish value insertion logic

--- src/main.py
+++ src/main.py
@@ -12,15 +12,22 @@ driver = webdriver.Firefox(executable_path=geckodriver_path, options=options)
 driver.get('https://investidorsardinha.r7.com/calculadoras/calculadora-de-juros-compostos/')
 
 start_value = driver.find_element(By.ID, 'input-jc-valor-inicial')
-start_value.send_keys('600000') # R$6000,00
+start_value.send_keys('100000') # R$1000,00
 
 monthly_value = driver.find_element(By.ID, 'input-jc-valor-mensal')
-monthly_value.send_keys('50000') # R$500,00
+monthly_value.send_keys('0') # R$0,00
 
 interest_rate = driver.find_element(By.ID, 'input-jc-taxa-juros')
-interest_rate.clear_field()
-interest_rate.send_keys('9')
+interest_rate.clear()
+interest_rate.send_keys('6')
 
 period = driver.find_element(By.ID, 'input-jc-periodo')
-period.clear_field()
-period.send_keys('1')
+period.clear()
+period.send_keys('4')
+
+buttons = driver.find_elements(By.CLASS_NAME, 'btn-primary')
+calculate_button = [x for x in buttons if x.get_attribute('value') == 'Calcular'][0]
+calculate_button.click()
+
+
+def blabla(x, y)
